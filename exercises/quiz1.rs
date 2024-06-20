ad58555a4c33500748d5b5496938d09540c27766
Thiago Massa <thiagown@gmail.com>
2024-06-20 23:07:05 +0200

    primitive types & vecs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(total: i32) -> i32 {
+    if (total > 40) {
+        total
+    } else {
+        total * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
